6935c74150498bb45e46d1c38f78026e1957bb17
tjdtnxx_TUF15 <nick76927@gmail.com>
2024-03-22 16:58:09 +0900

    2. 바퀴 움직임 끝

--- api_practice.py
+++ api_practice.py
@@ -2,6 +2,15 @@ from roboid import *
 
 beagle = Beagle()
 
-beagle.move_forward(1)
+beagle.wheels(50,50)    # 양쪽 바퀴를 50 %의 속력으로 전진한다.
 
-    
+
+wait(1000)
+
+beagle.wheels(-50,-50)  # 양쪽 바퀴를 50 %의 속력으로 후진한다.
+
+
+wait(1000)
+
+
+beagle.wheels(0,0)      # 양쪽 바퀴를 정지한다.
